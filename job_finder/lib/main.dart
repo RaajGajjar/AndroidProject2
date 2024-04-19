c2075aad81acd93f9b09b6df59f6a8a0b7b8c9c3
Raj Gajjar <rajgajjar3743@gmail.com>
2024-04-19 15:44:39 -0400

    Add code in Main.dart

--- job_finder/lib/main.dart
+++ job_finder/lib/main.dart
@@ -1,125 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:job_finder/screens/candidates_screen.dart';
+import 'package:job_finder/screens/home_screen.dart';
+import 'package:job_finder/screens/job_listings_screen.dart';
+import 'package:job_finder/screens/createPostScreen.dart';
+import 'package:job_finder/screens/user_data.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  UserData userData = UserData(prefs: prefs);
+  await userData.loadDataFromSharedPreferences();
+  runApp(MyApp(userData: userData));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final UserData userData;
 
-  // This widget is the root of your application.
+  const MyApp({Key? key, required this.userData}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Job Portal',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false, // Hide debug banner
+      home: MyHomePage(userData: userData),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  final UserData userData;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key, required this.userData}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedIndex = 0;
 
-  void _incrementCounter() {
+  void _onItemSelected(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
+    Navigator.pop(context); // Close the drawer
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    // List of screens to navigate based on the selected index
+    List<Widget> _screens = [
+      HomeScreen(userData: widget.userData), // Home Screen
+      CandidatesScreen(
+        userData: widget.userData,
+        onUpdateCandidates: _updatePosts, // Pass the correct function here
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+// Candidates Screen
+      JobListingsScreen(
+        jobListings: widget.userData.jobListings,
+        userData: widget.userData,
+      ), // Job Listings Screen
+      CreatePostScreen(
+        userData: widget.userData,
+        onUpdate: updateCandidates,
+      ), // Create Post Screen
+    ];
+
+    return WillPopScope(
+      onWillPop: () async {
+        if (_selectedIndex != 0) {
+          setState(() {
+            _selectedIndex = 0; // Navigate to Home Screen if not already on it
+          });
+          return false; // Prevent the app from closing
+        }
+        return true; // Allow the app to close
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Job Portal'), // App Bar Title
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    // Display the first user's circular image
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/images/post4.jpeg'),
+                      radius: 30, // Adjust the radius as needed
+                    ),
+                    SizedBox(height: 10),
+                    // Display the name of the first user
+                    Text(
+                      'lara leniy',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home'), // Home Screen Navigation
+                onTap: () => _onItemSelected(0),
+              ),
+              ListTile(
+                leading: Icon(Icons.people),
+                title: Text('All Candidates'), // Candidates Screen Navigation
+                onTap: () => _onItemSelected(1),
+              ),
+              ListTile(
+                leading: Icon(Icons.work),
+                title: Text('Job List'), // Job Listings Screen Navigation
+                onTap: () => _onItemSelected(2),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Post Jobs'), // Create Post Screen Navigation
+                onTap: () => _onItemSelected(3),
+              ),
+            ],
+          ),
+        ),
+        body: _screens[_selectedIndex], // Display selected screen
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  // Function to update the user's posts
+  void _updatePosts(List<Map<String, dynamic>> updatedCandidates) {
+    setState(() {
+      widget.userData.candidates = updatedCandidates;
+    });
+  }
+
+  // Function to update the list of candidates
+  void updateCandidates(List<Map<String, dynamic>> updatedCandidates) {
+    setState(() {
+      widget.userData.candidates = updatedCandidates;
+    });
   }
 }
